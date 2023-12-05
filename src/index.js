52291ab3771fa2d37a2fef9ded62590ea4be254f
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2023-12-05 13:14:37 +0100

    feature : implement time travel

--- src/index.js
+++ src/index.js
@@ -58,20 +58,20 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
 
     // Add some state to track which player is next and the history of moves
-    const [xIsNext, setXIsNext] = useState(true);
     const [history, setHistory] = useState([Array(9).fill(null)]);
-    const currentSquares = history[history.length - 1];
     const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
 
     // function that will be called by the Board component to update the game
     function handlePlay(nextSquares) {
-        setHistory([...history, nextSquares]);
-        setXIsNext(!xIsNext);
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
     }
 
     function jumpTo(nextMove) {
         setCurrentMove(nextMove);
-        setXIsNext(nextMove % 2 === 0);
     }
 
     // mapping over a history of moves
